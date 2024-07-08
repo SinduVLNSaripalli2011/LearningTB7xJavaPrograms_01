ea1eff9a8506889e24f5fa686af77fa6460a8029
Sindu Saripalli <sindu.pericherla@gmail.com>
2024-07-08 18:34:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#MArk down file
+------------------
+This repo Contains the source code for the exercisesthat we have done in ATB7X program
+
+# Java programs
+Core java
+OOPS
+Collections framework
+Multi Threading
+Design patterns
+File IO
+Other Misc concepts
---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,12 @@
+package ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+      //2+2? comment
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(3/2);
+    }
+}
